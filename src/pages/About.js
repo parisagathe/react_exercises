8460e67a471fb77b2fc58e3b8813e97590deb28b
parisagathe <parisagathepro@gmail.com>
2024-04-15 17:15:35 -0300

    Adding first version of the React country app exercise

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>À propos</h1>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi cupiditate totam vero quos laboriosam, repellendus obcaecati dolores vel earum nostrum assumenda praesentium possimus veniam! Blanditiis veritatis impedit animi unde consequuntur libero quia perferendis in sed fuga dolorem voluptas id dignissimos, eaque sit distinctio officia magni! Nulla iusto ea tenetur necessitatibus voluptas. Natus adipisci qui nemo eveniet similique sed. Repudiandae tenetur ipsam maxime incidunt, delectus tempore officiis ratione qui? Laborum incidunt quis accusantium odit itaque vero molestiae, possimus aspernatur. Vero expedita perspiciatis veritatis blanditiis architecto! Accusantium voluptatum odit eius facere consectetur corporis officiis cupiditate explicabo obcaecati, tenetur praesentium. Ut, itaque illo.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, voluptates? Amet laborum, ab mollitia corrupti vitae illum nesciunt optio rerum excepturi explicabo soluta ipsa illo esse rem consectetur at labore vel voluptates inventore. Repudiandae excepturi totam accusantium, qui inventore aspernatur cum harum, aliquam cupiditate enim labore non ut deleniti aliquid dolores quaerat tenetur mollitia id! Porro eligendi numquam reprehenderit necessitatibus illum saepe. Amet excepturi itaque quae a delectus eaque numquam debitis possimus. Non modi a corrupti quaerat assumenda dignissimos similique commodi ex? Corporis ab enim at odit, quam id iusto aperiam quia dolores numquam laborum a eius, illo autem porro veritatis voluptatem! Illo voluptatem modi nesciunt provident voluptatibus doloremque minima unde sit quia, consequuntur exercitationem quaerat nisi, earum, voluptas assumenda cumque. Et aliquid dolores similique magnam sint hic voluptates voluptatum enim saepe deserunt ut sunt pariatur voluptas quasi, vel harum tempore ab error distinctio praesentium earum est ducimus. Error voluptates animi sit molestias quidem. Provident, dolor quia? Quos ipsa necessitatibus repellendus iste velit accusantium nulla id soluta in beatae reiciendis repudiandae perspiciatis excepturi quam optio, atque corrupti enim ea ut autem, vel voluptas! Saepe perferendis dolorem alias deleniti, aperiam culpa corporis inventore pariatur dolorum vitae voluptatum excepturi, sint totam? Vel?</p>
+        </div>
+    );
+};
+
+export default About;
