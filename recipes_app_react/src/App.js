a68be879eae4d1c504d93ca0a519b53d622046b9
parisagathe <parisagathepro@gmail.com>
2024-04-17 15:07:11 -0300

    Developing the folders and files structure and adding the Routes in App.js

--- recipes_app_react/src/App.js
+++ recipes_app_react/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <h1>Bonjour !</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 };
 
