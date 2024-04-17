5413ea15c5d75d5074e70e407a2f535935fdc4a9
parisagathe <parisagathepro@gmail.com>
2024-04-17 15:56:43 -0300

    Reorganizing files

--- recipes_app_react/src/App.js
+++ recipes_app_react/src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Card from "./components/Card"
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="*" element={<Home />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <div>
+      <h1>Recipes App - React</h1>
+      <div className="cards-container">
+        <Card />
+      </div>
+    </div>
   );
 };
 
