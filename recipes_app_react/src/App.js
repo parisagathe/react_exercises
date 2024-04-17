c7f166ff0fb9c9953400f48079d46a9a9a87056b
parisagathe <parisagathepro@gmail.com>
2024-04-17 20:26:29 -0300

    Adding the final logic and style

--- recipes_app_react/src/App.js
+++ recipes_app_react/src/App.js
@@ -1,24 +1,47 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "./components/Card";
+import Error from "./components/Error";
 
 const App = () => {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=").then((res) => setData(res.data.meals));
-  }, []);
+  const getData = (search) => {
+    let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+    if (search) {
+      url = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + search;
+    }
+
+    axios.get(url)
+    .then((res) => {
+      if (!res.data) {
+        setData([]);
+      } else {
+        setData(res.data.meals);
+      }
+    });
+  };
+
+  useEffect(() => getData(), []);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
 
   return (
     <div>
       <h1>Recipes App - React</h1>
-      <form>
-        <input type="text" id="search" autoComplete="off" placeholder="Type the name of an ingredient" />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" id="search" autoComplete="off" placeholder="Type the name of an ingredient" onChange={(e) => getData(e.target.value)} />
       </form>
       <ul>
-          {data.map((meal, index) => (
+          {
+            data ? data
+          .map((meal, index) => (
             <Card key={index} meal={meal} />
-          ))}
+          )) : <Error /> }
         </ul>
     </div>
   );
---
+++ recipes_app_react/src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div className="no-results-error">
+            <p>No results found</p>
+        </div>
+    );
+};
+
+export default Error;
