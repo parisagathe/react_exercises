4b219f26ebb3e76c6c9eb43607b1cc3f4fb2566c
parisagathe <parisagathepro@gmail.com>
2024-04-22 18:00:03 -0300

    Adding initial recharts structure

---
+++ crypto_watch/src/components/GlobalChart.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GlobalChart = ({ coinsData }) => {
+    
+    return (
+        <div className="global-chart">
+            <h1>Test</h1>
+        </div>
+    );
+};
+
+export default GlobalChart;
