ff8f486f114111491092aff688938b3ea2bf14f5
parisagathe <parisagathepro@gmail.com>
2024-04-22 19:13:59 -0300

    Adding the Table Header logic

--- crypto_watch/src/App.js
+++ crypto_watch/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import HeaderInfos from "./components/HeaderInfos";
 import GlobalChart from "./components/GlobalChart";
 import axios from "axios";
+import Table from "./components/Table";
 
 const App = () => {
     const [coinsData, setCoinsData] = useState([]);
@@ -17,6 +18,7 @@ const App = () => {
                 <HeaderInfos />
                 <GlobalChart coinsData={coinsData} />
             </header>
+            <Table coinsData={coinsData} />
         </div>
     );
 };
---
+++ crypto_watch/src/components/Table.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const Table = ({ coinsData }) => {
+
+    const [rangeNumber, setRangeNumber] = useState(100);
+    const tableHeader = ["Prix", "MarketCap", "Volume", "1h", "1d", "1m", "1y", "ATH"];
+    const [orderBy, setOrderBy] = useState("");
+
+    return (
+        <div>
+            <div className="table-container">
+                <ul className="table-header">
+                    <div className="range-container">
+                        <span>
+                            Top{" "} <input type="text" value={rangeNumber} onChange={(e) => setRangeNumber(e.target.value)} />
+                        </span>
+                        <input type="range" min="1" max="250" value={rangeNumber} onChange={(e) => setRangeNumber(e.target.value)} />
+                    </div>
+                    {tableHeader.map((el) => (
+                        <li key={el}>
+                            <input type="radio" name="header-element" id={el} defaultChecked={el === orderBy || el === orderBy + "reverse" ? true : false} onClick={() => {
+                                if (orderBy === el) {
+                                    setOrderBy(el + "reverse")
+                                } else {
+                                    setOrderBy(el);
+                                }
+                            }} />
+                            <label htmlFor={el}>{el}</label>
+                        </li>
+                    ))}
+                </ul>
+                {coinsData && coinsData
+                .slice(0, rangeNumber)
+                .map((coin) => (
+                    <h1>{coin.symbol}</h1>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Table;
